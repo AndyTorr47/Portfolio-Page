b832dce07459d2c7efe0a3c07bea257c9e38a2b6
Andrew Torr <andrewtorr1@outlook.com>
2022-08-23 23:38:46 +0100

    ...

--- app.js
+++ app.js
@@ -16,6 +16,25 @@ function PageTransitions() {
       this.className += " active-btn";
     });
   }
+
+  //Sections Active
+  allSections.addEventListener("click", (e) => {
+    const id = e.target.dataset.id;
+    if (id) {
+      //remove selected from other button
+      sectBtns.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+      e.target.classList.add("active");
+
+      // hide other sections
+      sections.forEach((section) => {
+        section.classList.remove("active");
+      });
+      const element = document.getElementById(id);
+      element.classList.add("active");
+    }
+  });
 }
 
 PageTransitions();

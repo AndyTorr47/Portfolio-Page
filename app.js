46fcdfcdbcbcb71c94b8f2c5a7d8d33a21dc501a
Andrew Torr <andrewtorr1@outlook.com>
2022-08-31 13:22:19 +0100

    last few tocuhes before completion

--- app.js
+++ app.js
@@ -37,4 +37,11 @@ function PageTransitions() {
   });
 }
 
+//Toggle theme
+const themeBtn = document.querySelector(".theme-btn");
+themeBtn.addEventListener("click", () => {
+  let element = document.body;
+  element.classList.toggle("light-mode");
+});
+
 PageTransitions();
